9f8966da77635d26a45ad66a76b948050b8213ee
kethan187 <ketangoud938@gmail.com>
2026-07-30 06:21:27 +0530

    Fix DATABASE_URL string formatting in database.py

--- database.py
+++ database.py
@@ -1,7 +1,7 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker,declarative_base
 # DATABASE_URL = "mysql+pymysql://root:1234@localhost:3306/db"
-DATABASE_URL = mysql+pymysql://avnadmin:AVNS_3rQc2Bi7Er4norqWL9-@mysqldb-ketangoud938-6f1e.j.aivencloud.com:15620/defaultdb
+DATABASE_URL = "mysql+pymysql://avnadmin:AVNS_3rQc2Bi7Er4norqWL9-@mysqldb-ketangoud938-6f1e.j.aivencloud.com:15620/defaultdb"
 
 engine = create_engine(DATABASE_URL,connect_args={"ssl":{}})
 
